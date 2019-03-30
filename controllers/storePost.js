c7b637d912710d42ab5ee3f322013211be36b3c4
Siddarth Sharma <siddarthsharma1077@gmail.com>
2019-03-31 00:37:09 +0530

    Update storePost.js

--- controllers/storePost.js
+++ controllers/storePost.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const Post = require('../app/models/post')
+const Post = require('../app/models/Post')
 
 module.exports = (req, res) => {
   Post.create(req.body, (error, user) => {
