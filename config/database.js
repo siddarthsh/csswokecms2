aa391ec5105124dd9efdf4d565e130bd26818d1b
Siddarth Sharma <siddarthsharma1077@gmail.com>
2019-03-31 00:48:32 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-	'url' : 'mongodb://heroku_svfkrw48:chdsiddarth1077@ds111851.mlab.com:11851/heroku_svfkrw48' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	'url' : 'mongodb://<dbuser>:<dbpassword>@ds111851.mlab.com:11851/heroku_svfkrw48' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
