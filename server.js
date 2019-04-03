cd0d89ee8c0b6e89e0cc40973cd1526fb9418d8d
Siddarth Sharma <siddarthsharma1077@gmail.com>
2019-04-03 22:55:35 +0530

    NewRelicAddition

--- server.js
+++ server.js
@@ -2,45 +2,45 @@
 
 // set up ======================================================================
 // get all the tools we need
-var express  = require('express');
-const expressEdge = require('express-edge');
-var app      = express();
-var port     = process.env.PORT || 4000;
-var mongoose = require('mongoose');
-var passport = require('passport');
-var flash    = require('connect-flash');
+var express = require("express");
+const expressEdge = require("express-edge");
+var app = express();
+var port = process.env.PORT || 4000;
+var mongoose = require("mongoose");
+var passport = require("passport");
+var flash = require("connect-flash");
+require("newrelic");
 
-var configDB = require('./config/database.js');
+var configDB = require("./config/database.js");
 
 // configuration ===============================================================
-mongoose.connect(configDB.url, { useNewUrlParser: true })
-    .then(() => 'You are now connected to Mongo!')
-    .catch(err => console.error('Something went wrong', err))
- // connect to our database
+mongoose
+  .connect(configDB.url, { useNewUrlParser: true })
+  .then(() => "You are now connected to Mongo!")
+  .catch(err => console.error("Something went wrong", err));
+// connect to our database
 
-require('./config/passport')(passport); // pass passport for configuration
+require("./config/passport")(passport); // pass passport for configuration
 
+// set up our express application
+app.use(express.logger("dev")); // log every request to the console
+app.use(express.cookieParser()); // read cookies (needed for auth)
+app.use(express.bodyParser()); // get information from html forms
+app.use(express.static("public"));
+app.use(expressEdge);
+app.set("views", __dirname + "/views"); // set up ejs for templating
 
-	// set up our express application
-	app.use(express.logger('dev')); // log every request to the console
-	app.use(express.cookieParser()); // read cookies (needed for auth)
-	app.use(express.bodyParser()); // get information from html forms
-	app.use(express.static('public'));
-	app.use(expressEdge);
-	app.set('views',__dirname+'/views'); // set up ejs for templating
-
-	// required for passport
-	app.use(express.session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
-	app.use(passport.initialize());
-	app.use(passport.session()); // persistent login sessions
-	app.use(flash()); // use connect-flash for flash messages stored in session
-
+// required for passport
+app.use(express.session({ secret: "ilovescotchscotchyscotchscotch" })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 
 // routes ======================================================================
-require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+require("./app/routes.js")(app, passport); // load our routes and pass in our app and fully configured passport
 
 // launch ======================================================================
 
 app.listen(port, () => {
-    console.log("App is running on port " + port);
+  console.log("App is running on port " + port);
 });
