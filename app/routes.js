c40c10b75b4c053925a3f1ff4d4366920fc4999f
Siddarth Sharma <siddarthsharma1077@gmail.com>
2019-04-06 08:51:06 +0530

    Dashboard v0.1.4.2
    1) Minor Tweaks in Design (Padding property of the viewposts and viewusers pages,to improve view for mobile users)
    2) Changed the link of the getting started button to /signup from /login

--- app/routes.js
+++ app/routes.js
@@ -35,7 +35,7 @@ module.exports = function(app, passport) {
   app.post(
     "/login",
     passport.authenticate("local-login", {
-      successRedirect: "/profile", // redirect to the secure profile section
+      successRedirect: "/", // redirect to the secure profile section
       failureRedirect: "/login", // redirect back to the signup page if there is an error
       failureFlash: true // allow flash messages
     })
@@ -54,7 +54,7 @@ module.exports = function(app, passport) {
   app.post(
     "/signup",
     passport.authenticate("local-signup", {
-      successRedirect: "/profile", // redirect to the secure profile section
+      successRedirect: "/", // redirect to the secure profile section
       failureRedirect: "/signup", // redirect back to the signup page if there is an error
       failureFlash: true // allow flash messages
     })
